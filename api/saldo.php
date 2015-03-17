893a085fb7f69a7de9460af142de660615c48182
root <root@ubuntu-vm.(none)>
2015-03-17 15:38:30 +0100

    added check

--- api/saldo.php
+++ api/saldo.php
@@ -4,6 +4,10 @@ $link=mysqli_connect("localhost","root","skere","SkereDB");
 
 $br=$_GET["br"];
 
+if (preg_match("/x^\d+$/mg", $br)){
+	die("rekeningnummer kan alleen cijfers bevatten");
+}
+
 $result=mysqli_query($link,"SELECT `Rekening_saldo` FROM `Rekening` WHERE `Rekening_nr` = ".$br." LIMIT 1 ") or die(mysqli_error($link));
 $temp=mysqli_fetch_assoc($result);
 $saldo = $temp["Rekening_saldo"];
