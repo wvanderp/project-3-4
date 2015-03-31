8ac166e8e0d2160d7714036f3a39942114af3b80
root <root@ubuntu-vm.(none)>
2015-03-31 14:53:25 +0200

    chaged get var

--- api/transactie.php
+++ api/transactie.php
@@ -3,7 +3,7 @@ include("utils.php");
 
 $link = mysqli_connect("localhost","root","skere","SkereDB");
 
-$rekening_nr = $_GET["br"];
+$rekening_nr = $_GET["rekening_nr"];
 
 $rekening_nr = mysqli_real_escape_string($link, $rekening_nr);
 
