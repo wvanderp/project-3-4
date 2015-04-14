c8618793ac61627734c60fff32695067426bff4c
wouter van der plas <wouterv.dplas@gmail.com>
2015-04-14 12:57:01 +0200

    made a test print working

--- node printer/print.js
+++ node printer/print.js
@@ -1,4 +1,16 @@
+var printer = require("printer");
+var util = require('util');
 
-var printer = require("../lib"),
-    util = require('util');
-console.log("installed printers:\n"+util.inspect(printer.getPrinters(), {colors:true, depth:10}));
+// var print = 'CutePDF Writer'; //for test purpusus
+var print = 'DYMO LabelWriter 400'; // for some real printing
+
+// console.log("installed printers:\n"+util.inspect(printer.getPrinters(), {colors:true, depth:10}));
+
+printer.printDirect({data:" print from  Node.JS buffer" // or simple String: "some text"
+	, printer: print // printer name, if missing then will print to default printer
+	, type: 'TEXT' // type: RAW, TEXT, PDF, JPEG, .. depends on platform
+	, success:function(jobID){
+		console.log("sent to printer with ID: "+jobID);
+	}
+	, error:function(err){console.log(err);}
+});
