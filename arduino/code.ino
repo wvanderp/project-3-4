1e4018a3e3baf28920a428feb07048c2523da1b7
Rubenb94 <rbuitendijk94@gmail.com>
2015-04-24 10:15:41 +0200

    Card number zonder spaties
    syyy syy syyncc that bitch!!!!

--- arduino/code.ino
+++ arduino/code.ino
@@ -100,7 +100,7 @@ void loop()
          
          Serial.print(F("Card:"));
         for (byte i = 0; i < mfrc522.uid.size; i++) {
-                Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+                Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : "");
                 Serial.print(mfrc522.uid.uidByte[i], HEX);
         } 
         Serial.println();
