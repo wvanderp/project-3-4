99a83a7cfc3c169cd08c74438b6cff562721a5b6
root <root@ubuntu-vm.(none)>
2015-06-02 16:23:00 +0200

    login.php verbeterd en balance.php bezig

--- api/balance.php
+++ api/balance.php
@@ -1,11 +1,10 @@
 <?php
-
 include("utils.php");
 require_once 'errorGen.php';
 
 $link = mysqli_connect("localhost","root","skere","SkereDB");
 
-$token = 
+$token = 132;
 $query = "SELECT `validatieToken` FROM `validatie` WHERE `validatieToken` = ".$token." LIMIT 1 ";
 
 ?>
--- api/login.php
+++ api/login.php
@@ -6,26 +6,38 @@ $link = mysqli_connect("localhost","root","skere","SkereDB");
 
 $pin = $_GET["pin"];
 
-$pasnr = $_GET["pasnr"];
+$cardId = $_GET["cardId"];
+
+$query = "SELECT `pas_id` FROM `pas` WHERE `pas_id` = '".$cardId."' LIMIT 1";
 
 $query = "SELECT `pas_id` FROM `pas` WHERE `pas_id` = ".$pasnr." LIMIT 1";
 mysqli_query($link, $query);
 
-$query2 = "SELECT `pincode`FROM `pas`WHERE `pincode` = ".$pin." LIMIT 1";
+$query2 = "SELECT `pincode` FROM `pas` WHERE `pincode` = '".$pin."' LIMIT 1";
+mysqli_query($link, $query2);
+
+$failedattempts = "SELECT `failedattempts` FROM `pas` WHERE `pas_id` = '".$cardId."' LIMIT 1" LIMIT 1") or die(mysqli_error($link));
+
+if ($cardId == $query && $pin == $query2){
+        $succes = 1234;
+        mysqli_query($link, "UPDATE `pas` SET `failedattempts` = 0 WHERE `pas_id` = '".$cardId."' LIMIT 1") or die(mysqli_error($link));
 
 if ($pasnr == $query && $pin == $query2){
-	$token = md5(time());
-	$responce = array(
-		"sucsess" => array("token" => $token ),
-		"error" => array()
-	)
+        $token = md5(time());
+        $responce = array(
+                "sucsess" => array("token" => $token ),
+                "error" => array()
+        )
+
 }else {
-	$error = 201;
+        $error = 201;
+        if($cardId == $query){
+                $failedattempts = $failedattempts+1;
+                mysqli_query($link, "UPDATE `pas` SET `failedattempts` = '".$failedattempts."' LIMIT 1") or die(mysqli_error($link));
+        }
 }
 
 
 $json = json_encode($responce);
 ?>
-
-
 
