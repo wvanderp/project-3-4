b2fa278abcbd1b50458f2661b18bcef402a95112
wouter van der plas <wouterv.dplas@gmail.com>
2015-06-09 13:26:54 +0200

    fist commit

--- web/js/bon.js
+++ web/js/bon.js
@@ -2,26 +2,10 @@
 // var util = require('util');
 
 function printBon() {
-// 		var newsaldo = saldo - amount;
-// 		var print = "skere bank\n rekening: "+bankrekening+"\n saldo: "+newsaldo+"\n afgenomen: "+amount+"\n Bedankt en tot ziens";
+		var newsaldo = saldo - amount;
+		var print = "skere bank\n rekening: "+bankrekening+"\n saldo: "+newsaldo+"\n afgenomen: "+amount+"\n Bedankt en tot ziens";
 
-// 		io.on("connect", function (socket) {
-// 			console.log("Socket connected");
-// 			socket.on("keystroke", function (stroke){
-// 				if (stroke !== last) {
-// 					last = stroke;
-// 					intervalId = setInterval(resetLast, 300);
-// 					console.log(stroke);
-// 					keypad(stroke);
-// 				};
-// 			})
-// 			socket.on("card", function (card) {
-// 				if (card !== last) {
-// 					last = card;
-// 					intervalId = setInterval(resetLast, 300);
-// 					console.log(card);
-// 					loadCard(card);
-// 				}
-// 			})
-// 		});
+		io.on("connect", function (socket) {
+			io.emit("card", result.substring(6));
+		});
 }
