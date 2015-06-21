6277e216ef92399d428146f8a2ae8ebbe6b8fcf4
Rubenb94 <rbuitendijk94@gmail.com>
2015-06-21 13:54:55 +0200

    arduino niewe card reader
    om de kaart uit te lezen

--- arduino/code.ino
+++ arduino/code.ino
@@ -1,64 +1,49 @@
- /*
- ********rfid reader*****
- * RST/Reset   RST          9            
- * SPI SS      SDA(SS)      10 
- * SPI MOSI    MOSI         11 
- * SPI MISO    MISO         12 
- * SPI SCK     SCK          13
-*/
-
 #include <SPI.h>//include the SPI bus library
 #include <MFRC522.h>//include the RFID reader library
 #include <Keypad.h>
-
 #define ledpin 8
 #define SS_PIN 10  //slave select pin
 #define RST_PIN A0  //reset pin
 MFRC522 mfrc522(SS_PIN, RST_PIN);        // instatiate a MFRC522 reader object.
 MFRC522::MIFARE_Key key;//create a MIFARE_Key struct named 'key', which will hold the card information
-
 const byte ROWS = 4; // Four rows
 const byte COLS = 4; // Three columns
-
-// Define the Keymap
 char keys[COLS][ROWS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'N','0','J','D'}
-};
-// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 8, 7, 6, 5 };
-// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+};byte rowPins[ROWS] = { 8, 7, 6, 5 };// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
 byte colPins[COLS] = { 4,3,2,9 }; 
-
-// Create the Keypad
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 
+
 void setup() {
-        pinMode(ledpin,OUTPUT);
-        digitalWrite(ledpin, HIGH);
-        Serial.begin(9600);        // Initialize serial communications with the PC
+        Serial.begin(115200);        // Initialize serial communications with the PC
         SPI.begin();               // Init SPI bus
         mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
-         
         Serial.println("Scan a MIFARE Classic card");
-       
+        
         // Prepare the security key for the read and write functions - all six key bytes are set to 0xFF at chip delivery from the factory.
         // Since the cards in the kit are new and the keys were never defined, they are 0xFF
         // if we had a card that was programmed by someone else, we would need to know the key to be able to access it. This key would then need to be stored in 'key' instead.
  
-       
+        for (byte i = 0; i < 6; i++) {
+                key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+        }
 
 }
 
+int block=2;//this is the block number we will write into and then read. Do not write into 'sector trailer' block, since this can make the block unusable.
 
-
+//an array with 16 bytes to be written into one of the 64 card blocks is defined
+//byte blockcontent[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//all zeros. This can be used to delete a block.
+byte readbackblock[18];//This array is used for reading out a block. The MIFARE_Read method requires a buffer that is at least 18 bytes to hold the 16 bytes of a block.
 
 void loop()
 {
- char key = kpd.getKey();
+  char key = kpd.getKey();
   if(key)  // Check for a valid key.
   {
     switch (key)
@@ -73,9 +58,7 @@ void loop()
         Serial.println(key);
     }
   }
-   
-   
-   
+
         /*****************************************establishing contact with a tag/card**********************************************************************/
         
   	// Look for new cards (in case you wonder what PICC means: proximity integrated circuit card)
@@ -88,30 +71,15 @@ void loop()
 		return;//if it returns a '0' something went wrong and we return to the start of the loop
 	}
 
-        // Among other things, the PICC_ReadCardSerial() method reads the UID and the SAK (Select acknowledge) into the mfrc522.uid struct, which is also instantiated
-        // during this process.
-        // The UID is needed during the authentication process
-        	//The Uid struct:
-	        //typedef struct {
-		//byte		size;			// Number of bytes in the UID. 4, 7 or 10.
-		//byte		uidByte[10];            //the user ID in 10 bytes.
-		//byte		sak;			// The SAK (Select acknowledge) byte returned from the PICC after successful selection.
-	        //} Uid;
+   
+         readBlock(block, readbackblock);//read the block back
+         Serial.print("Card:");
+         for (int j=0 ; j<16 ; j++)//print the block contents
+         {
+           Serial.write (readbackblock[j]);//Serial.write() transmits the ASCII numbers as human readable characters to serial monitor
+         }
+         Serial.println("");
          
-         Serial.print(F("Card:"));
-        for (byte i = 0; i < mfrc522.uid.size; i++) {
-                Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : "");
-                Serial.print(mfrc522.uid.uidByte[i], HEX);
-        } 
-        Serial.println();
-        delay(2000);
-        /*****************************************writing and reading a block on the card**********************************************************************/
-         
-         
-         
-  
-
-
  
 }
 
